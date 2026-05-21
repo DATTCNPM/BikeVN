0c4c5b689f233155200164af673ff2bdcc8406fc
Hoang Phuong <155792523+DiazMarco118@users.noreply.github.com>
2026-05-21 17:32:10 +0700

    feat(exception): add AppException and ErrorCode classes for error handling

---
+++ backend/src/main/java/com/backend/bikerental/exception/AppException.java
@@ -0,0 +1,15 @@
+package com.backend.bikerental.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AppException extends RuntimeException {
+    private ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
